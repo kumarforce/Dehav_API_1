8f6e9d2e7f7ea562da9c51fdc84d50b806670bfb
kumarforce <kumarforce7@gmail.com>
2019-06-19 21:22:40 +0530

    Add files via upload

---
+++ BioClassData.java
@@ -0,0 +1,18 @@
+public class BioDataWrapper{
+
+public  	String 	SqlId	{get;set;}
+public  	String 	PolicyInterests	{get;set;}
+public  	String 	CurrentAllPartyParilamentaryGroupAPPGMemberships	{get;set;}
+public  	String 	PartyPosts	{get;set;}
+public  	String 	ElectionToParliament	{get;set;}
+public  	String 	LifeBeforePariament	{get;set;}
+public  	String 	PersonalLife	{get;set;}
+public  	String 	Education	{get;set;}
+public  	String 	ProfessionalLife	{get;set;}
+public  	String 	PublicBodies	{get;set;}
+public  	String 	ImageUrl	{get;set;}
+public  	String 	Domain {get;set;}
+
+public BioDataWrapper(){}
+
+}
---
+++ PeoplePointData.java
@@ -0,0 +1,50 @@
+public class PeoplePointData{
+
+	public String 	SqlId	{get;set;}
+	public String 	Firstname	{get;set;}
+	public String 	Surname	{get;set;}
+	public String 	TypeCodeName	{get;set;}
+	public String 	FullName	{get;set;}
+	public String 	AddressableTitle	{get;set;}
+	public String 	ShortTitle	{get;set;}
+	public String 	ConstituencyProfile	{get;set;}
+	public String 	CurrentGovernmentOrShadowFrontBenchPosition	{get;set;}
+	public String 	PersonalTitle	{get;set;}
+	public String 	Party	{get;set;}
+	public String 	Constituency	{get;set;}
+	public String 	GSSCode	{get;set;}
+	public String 	EnglishRegion	{get;set;}
+	public String 	DateOfBirth	{get;set;}
+	public String 	Majority	{get;set;}
+	public String 	VotesAtLastElection	{get;set;}
+	public String 	MajorityChangeAtLastElection	{get;set;}
+	public String 	Country	{get;set;}
+	public String 	OfficeAddress	{get;set;}
+	public String 	OfficeTelephone	{get;set;}
+	public String 	OfficeFax	{get;set;}
+	public String 	Email	{get;set;}
+	public String 	Organisation	{get;set;}
+	public String 	MinisterialTitle	{get;set;}
+	public String 	MinisterialAddress	{get;set;}
+	public String 	MinisterialTelephone	{get;set;}
+	public String 	PresentSelectCommitteeMemberships	{get;set;}
+	public String 	MinisterialFax	{get;set;}
+	public String 	MinisterialEmail	{get;set;}
+	public String 	DiarySecretaryEmail	{get;set;}
+	public String 	ConstituencyAddress	{get;set;}
+	public String 	ConstituencyTelephone	{get;set;}
+	public String 	ConstituencyFax	{get;set;}
+	public String 	HomeTelephone	{get;set;}
+	public String 	Mobile	{get;set;}
+	public String 	PreferredMethodofContact	{get;set;}
+	public String 	Twitter	{get;set;}
+	public String 	Facebook	{get;set;}
+	public String 	LinkedIn	{get;set;}
+	public String 	RSSLink	{get;set;}
+	public String 	Gender	{get;set;}
+	public String 	Cabinet_ShadowCabinet	{get;set;}
+	public String 	JuniorMinister_FrontBenchSpokesperson	{get;set;}
+	
+	public PeoplePointData(){}
+
+}
